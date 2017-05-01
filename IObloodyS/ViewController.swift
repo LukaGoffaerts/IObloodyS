5a01a48a056f5e3fbfc682372697ad765cab34d0
Luka Goffaerts <lukagoffaerts@hotmail.com>
2017-05-01 20:50:42 +0200

    Initial Commit

---
+++ IObloodyS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IObloodyS
+//
+//  Created by Luka Goffaerts on 1/05/17.
+//  Copyright © 2017 Luka Goffaerts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
