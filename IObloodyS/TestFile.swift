2a2935ebb79e9a19f1b6d732e826a4996114b2e3
Luka Goffaerts <lukagoffaerts@hotmail.com>
2017-05-01 20:55:49 +0200

    First testfile made

---
+++ IObloodyS/TestFile.swift
@@ -0,0 +1,14 @@
+//
+//  TestFile.swift
+//  IObloodyS
+//
+//  Created by Luka Goffaerts on 1/05/17.
+//  Copyright © 2017 Luka Goffaerts. All rights reserved.
+//
+
+import Foundation
+
+
+print("Test")
+
+
